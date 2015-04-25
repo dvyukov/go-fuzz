ba38eee6733603ea0f6d3a86695a5608e54de9a6
Dmitry Vyukov <dvyukov@google.com>
2015-04-25 14:22:51 +0300

    disable build of nss example it won't build as is

--- examples/nss/nss.go
+++ examples/nss/nss.go
@@ -1,3 +1,8 @@
+// This example won't build as is, because ParseNSSConf function is not exported
+// from net package. To build this example, you need to patch net package to
+// rename parseNSSConf to ParseNSSConf first.
+// +build never
+
 package nss
 
 import (
