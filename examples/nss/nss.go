c1b4cec82df8341d09ac6441daad754390ce13d2
Dmitry Vyukov <dvyukov@google.com>
2015-04-25 14:25:00 +0300

    build tags don't affect go get so delete spdy example and comment code in nss example

--- examples/nss/nss.go
+++ examples/nss/nss.go
@@ -1,8 +1,3 @@
-// This example won't build as is, because ParseNSSConf function is not exported
-// from net package. To build this example, you need to patch net package to
-// rename parseNSSConf to ParseNSSConf first.
-// +build never
-
 package nss
 
 import (
@@ -11,8 +6,13 @@ import (
 )
 
 func Fuzz(data []byte) int {
+	// This example won't build as is, because ParseNSSConf function is not exported
+	// from net package. To build this example, you need to patch net package to
+	// rename parseNSSConf to ParseNSSConf first.
+	/*
 	if net.ParseNSSConf(bytes.NewReader(data)) == nil {
 		return 0
 	}
+	*/
 	return 1
 }
