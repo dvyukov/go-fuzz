3a458a9fd027ad9e90687f5009b3f90ca1cfb7e9
Dmitry Vyukov <dvyukov@google.com>
2015-05-07 21:10:36 +0300

    sanity check image size in image decoding tests this prevents hangs and OOMs on insanely large images

--- examples/png/png.go
+++ examples/png/png.go
@@ -6,6 +6,13 @@ import (
 )
 
 func Fuzz(data []byte) int {
+	cfg, err := png.DecodeConfig(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+	if cfg.Width*cfg.Height > 1e6 {
+		return 0
+	}
 	img, err := png.Decode(bytes.NewReader(data))
 	if err != nil {
 		return 0
