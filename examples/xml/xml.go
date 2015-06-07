0509ca50061796384223ec2d69857ed65132f044
dvyukov <dvyukov@localhost>
2015-06-07 20:11:32 +0200

    extend xml test

--- examples/xml/xml.go
+++ examples/xml/xml.go
@@ -3,54 +3,65 @@ package xml
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
+	"reflect"
 )
 
 type X struct {
 	A int     `xml:"a,attr"`
 	B float64 `xml:"B"`
 	C bool    `xml:"C>CC"`
-	D string  `xml:",comment"`
-	E []byte  `xml:",innerxml"`
+	D string  `xml:",comment,omitempty"`
+	E []byte  `xml:",innerxml,omitempty"`
 	F *X
-	G string `xml:",any"`
-	H string `xml:"-"`
-	J []byte `xml:",chardata"`
+	G string `xml:",any,omitempty"`
+	H string `xml:"-,omitempty"`
+	J []byte `xml:",chardata,omitempty"`
 }
 
 func Fuzz(data []byte) int {
 	score := 0
-	if test(data, nil) {
+	for _, ctor := range []func() interface{}{
+		func() interface{} { return nil },
+		func() interface{} { return new(string) },
+		func() interface{} { return []string{} },
+		func() interface{} { return new(X) },
+	} {
+		v := ctor()
+		valid := false
+		dec := xml.NewDecoder(bytes.NewReader(data))
+		if dec.Decode(v) == nil {
+			valid = true
+		}
+		dec1 := xml.NewDecoder(bytes.NewReader(data))
+		dec1.Strict = false
+		dec1.AutoClose = xml.HTMLAutoClose
+		dec1.Entity = xml.HTMLEntity
+		if dec1.Decode(v) != nil {
+			if valid {
+				panic("non-strict mode is weaker than strict")
+			}
+			continue
+		}
 		score = 1
-	}
-	var s string
-	if test(data, &s) {
-		score = 1
-	}
-	var a []string
-	if test(data, &a) {
-		score = 1
-	}
-	var x X
-	if test(data, &x) {
-		score = 1
-		xml.Marshal(&x)
+		data1, err := xml.Marshal(v)
+		if err != nil {
+			panic(err)
+		}
+
+		v1 := ctor()
+		dec2 := xml.NewDecoder(bytes.NewReader(data1))
+		dec2.Strict = false
+		dec2.AutoClose = xml.HTMLAutoClose
+		dec2.Entity = xml.HTMLEntity
+		if err := dec2.Decode(v1); err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(v, v1) {
+			fmt.Printf("v0: %+v\n", v)
+			fmt.Printf("v1: %+v\n", v)
+			panic("non-idempotent Marshal/Unmarshal")
+		}
 	}
 	return score
-}
-
-func test(data []byte, v interface{}) (valid bool) {
-	dec := xml.NewDecoder(bytes.NewReader(data))
-	if dec.Decode(v) == nil {
-		valid = true
-	}
-	dec1 := xml.NewDecoder(bytes.NewReader(data))
-	dec1.Strict = false
-	dec1.AutoClose = xml.HTMLAutoClose
-	dec1.Entity = xml.HTMLEntity
-	if dec1.Decode(v) == nil {
-		valid = true
-	} else if valid {
-		panic("non-strict mode is weaker than strict")
-	}
-	return
 }
