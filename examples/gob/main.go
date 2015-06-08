248c12a6beedfb4c2c25117b92223f4131f5c042
Dmitry Vyukov <dvyukov@gmail.com>
2015-06-08 19:20:57 +0200

    extend gob test

--- examples/gob/main.go
+++ examples/gob/main.go
@@ -3,6 +3,7 @@ package gob
 import (
 	"bytes"
 	"encoding/gob"
+	"fmt"
 	"reflect"
 )
 
@@ -11,7 +12,13 @@ type X struct {
 	B string
 	C float64
 	D []byte
-	I interface{}
+	E interface{}
+	F complex128
+	G []interface{}
+	H *int
+	I **int
+	J *X
+	K map[string]int
 }
 
 func init() {
@@ -20,35 +27,59 @@ func init() {
 
 func Fuzz(data []byte) int {
 	score := 0
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(nil) == nil {
-		score++
-	}
-	i := 0
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&i) == nil {
-		score++
-	}
-	s := ""
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&s) == nil {
-		score++
-	}
-	var b []byte
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&b) == nil {
-		score++
-	}
-	var f float64
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&f) == nil {
-		score++
-	}
-	var x X
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&x) == nil {
-		score++
-	}
-	var v reflect.Value
-	if gob.NewDecoder(bytes.NewReader(data)).Decode(&v) == nil {
-		score++
-	}
-	if gob.NewDecoder(bytes.NewReader(data)).DecodeValue(v) == nil {
-		score++
+	for _, ctor := range []func() interface{}{
+		func() interface{} { return nil },
+		func() interface{} { return new(int) },
+		func() interface{} { return new(string) },
+		func() interface{} { return new(float64) },
+		func() interface{} { return []byte{} },
+		func() interface{} { return new([]byte) },
+		func() interface{} { return []interface{}{} },
+		func() interface{} { return new(interface{}) },
+		func() interface{} { return new(complex128) },
+		func() interface{} { return make(map[int]int) },
+		func() interface{} { return make(map[string]interface{}) },
+		func() interface{} { return new(X) },
+	} {
+		v := ctor()
+		if gob.NewDecoder(bytes.NewReader(data)).Decode(v) != nil {
+			continue
+		}
+		score = 1
+		if ctor() == nil {
+			continue
+		}
+		b1 := new(bytes.Buffer)
+		if err := gob.NewEncoder(b1).Encode(v); err != nil {
+			panic(err)
+		}
+		v1 := reflect.ValueOf(ctor())
+		err := gob.NewDecoder(bytes.NewReader(data)).DecodeValue(v1)
+		if err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(v, v1.Interface()) {
+			fmt.Printf("v0: %#v\n", v)
+			fmt.Printf("v1: %#v\n", v1.Interface())
+			panic("values not equal")
+		}
+		b2 := new(bytes.Buffer)
+		err = gob.NewEncoder(b2).EncodeValue(v1)
+		if err != nil {
+			panic(err)
+		}
+		if !bytes.Equal(b1.Bytes(), b2.Bytes()) {
+			panic("bytes are not equal")
+		}
+		v2 := ctor()
+		if err := gob.NewDecoder(b1).Decode(v2); err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(v, v2) {
+			fmt.Printf("v0: %#v\n", v)
+			fmt.Printf("v2: %#v\n", v2)
+			panic("values not equal 2")
+		}
 	}
 	return score
 }
