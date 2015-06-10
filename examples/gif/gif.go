94b4e7af1a1ebb39070e1d01807b1061e8ef6dcf
Dmitry Vyukov <dvyukov@gmail.com>
2015-06-10 22:05:15 +0200

    extend gif test

--- examples/gif/gif.go
+++ examples/gif/gif.go
@@ -2,7 +2,9 @@ package gif
 
 import (
 	"bytes"
+	"fmt"
 	"image/gif"
+	"reflect"
 )
 
 func Fuzz(data []byte) int {
@@ -17,10 +19,21 @@ func Fuzz(data []byte) int {
 	if err != nil {
 		return 0
 	}
-	var w bytes.Buffer
-	err = gif.Encode(&w, img, nil)
-	if err != nil {
-		panic(err)
+	for c := 1; c <= 256; c += 21 {
+		var w bytes.Buffer
+		err = gif.Encode(&w, img, &gif.Options{NumColors: c})
+		if err != nil {
+			panic(err)
+		}
+		img1, err := gif.Decode(&w)
+		if err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(img.Bounds(), img1.Bounds()) {
+			fmt.Printf("img0: %#v\n", img.Bounds())
+			fmt.Printf("img1: %#v\n", img1.Bounds())
+			panic("bounds changed")
+		}
 	}
 	return 1
 }
