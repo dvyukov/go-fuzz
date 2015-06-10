d5c70e0f68c853bcc5254a6f0c9bd05811e5127f
Dmitry Vyukov <dvyukov@gmail.com>
2015-06-10 15:54:16 +0200

    extend png test

--- examples/png/png.go
+++ examples/png/png.go
@@ -2,7 +2,9 @@ package png
 
 import (
 	"bytes"
+	"fmt"
 	"image/png"
+	"reflect"
 )
 
 func Fuzz(data []byte) int {
@@ -17,10 +19,22 @@ func Fuzz(data []byte) int {
 	if err != nil {
 		return 0
 	}
-	var w bytes.Buffer
-	err = png.Encode(&w, img)
-	if err != nil {
-		panic(err)
+	for _, c := range []png.CompressionLevel{png.DefaultCompression, png.NoCompression, png.BestSpeed, png.BestCompression} {
+		var w bytes.Buffer
+		e := &png.Encoder{c}
+		err = e.Encode(&w, img)
+		if err != nil {
+			panic(err)
+		}
+		img1, err := png.Decode(&w)
+		if err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(img.Bounds(), img1.Bounds()) {
+			fmt.Printf("bounds0: %#v\n", img.Bounds())
+			fmt.Printf("bounds1: %#v\n", img1.Bounds())
+			panic("bounds have changed")
+		}
 	}
 	return 1
 }
