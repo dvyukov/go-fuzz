ef25089870f79f9967c2f1b84b618a19334a0e77
Dmitry Vyukov <dvyukov@gmail.com>
2015-06-10 21:36:08 +0200

    extend jpeg test

--- examples/jpeg/jpeg.go
+++ examples/jpeg/jpeg.go
@@ -3,6 +3,7 @@ package jpeg
 import (
 	"bytes"
 	"image/jpeg"
+	"reflect"
 )
 
 func Fuzz(data []byte) int {
@@ -17,10 +18,19 @@ func Fuzz(data []byte) int {
 	if err != nil {
 		return 0
 	}
-	var w bytes.Buffer
-	err = jpeg.Encode(&w, img, nil)
-	if err != nil {
-		panic(err)
+	for q := 0; q <= 100; q += 10 {
+		var w bytes.Buffer
+		err = jpeg.Encode(&w, img, &jpeg.Options{q})
+		if err != nil {
+			panic(err)
+		}
+		img1, err := jpeg.Decode(&w)
+		if err != nil {
+			panic(err)
+		}
+		if !reflect.DeepEqual(img.Bounds(), img1.Bounds()) {
+			panic("bounds changed")
+		}
 	}
 	return 1
 }
