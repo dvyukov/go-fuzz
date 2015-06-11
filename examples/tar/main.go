62d37f5e875de204330cba588ea59bba32ce3fde
Dmitry Vyukov <dvyukov@gmail.com>
2015-06-11 21:54:38 +0200

    extend tar test

--- examples/tar/main.go
+++ examples/tar/main.go
@@ -5,17 +5,84 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+
+	"github.com/dvyukov/go-fuzz/examples/fuzz"
 )
 
 func Fuzz(data []byte) int {
 	t := tar.NewReader(bytes.NewReader(data))
-	score := 0
+	var headers []*tar.Header
+	var contents [][]byte
 	for {
-		_, err := t.Next()
-		if err != nil {
-			return score
+		hdr, err := t.Next()
+		if err == io.EOF {
+			break
 		}
-		io.Copy(ioutil.Discard, t)
-		score++
+		if err != nil {
+			return 0
+		}
+		if len(hdr.Name) > 1e6 ||
+			len(hdr.Linkname) > 1e6 ||
+			len(hdr.Uname) > 1e6 ||
+			len(hdr.Gname) > 1e6 ||
+			len(hdr.Xattrs) > 1e6 {
+			panic("huge header data")
+		}
+		if hdr.Size > 1e6 {
+			panic("huge claimed file size")
+		}
+		fdata, err := ioutil.ReadAll(t)
+		if err != nil {
+			return 0
+		}
+		if int64(len(fdata)) > hdr.Size {
+			panic("long read")
+		}
+		if int64(len(fdata)) < hdr.Size {
+			panic("short read")
+		}
+		hdr1 := *hdr // make a copy to be safe
+		headers = append(headers, &hdr1)
+		contents = append(contents, fdata)
 	}
+	buf := new(bytes.Buffer)
+	w := tar.NewWriter(buf)
+	for i, hdr := range headers {
+		err := w.WriteHeader(hdr)
+		if err != nil {
+			panic(err)
+		}
+		n, err := w.Write(contents[i])
+		if err != nil {
+			panic(err)
+		}
+		if n != len(contents[i]) {
+			panic("short write")
+		}
+	}
+	err := w.Close()
+	if err != nil {
+		panic(err)
+	}
+	t1 := tar.NewReader(buf)
+	for i := 0; ; i++ {
+		hdr, err := t1.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fdata, err := ioutil.ReadAll(t)
+		if err != nil {
+			panic(err)
+		}
+		if !fuzz.DeepEqual(hdr, headers[i]) {
+			panic("headers diffs")
+		}
+		if !bytes.Equal(fdata, contents[i]) {
+			panic("data diffs")
+		}
+	}
+	return 1
 }
