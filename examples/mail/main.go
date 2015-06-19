9163d30146a7c19ab07b168bd673c7cb3ebdf512
Dmitry Vyukov <dvyukov@google.com>
2015-06-19 15:42:38 +0200

    add net/mail test

---
+++ examples/mail/main.go
@@ -0,0 +1,34 @@
+package mail
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/mail"
+	"reflect"
+)
+
+func Fuzz(data []byte) int {
+	msg, err := mail.ReadMessage(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+	msg.Header.AddressList("to")
+	msg.Header.Date()
+	if addr, err := mail.ParseAddress(msg.Header.Get("from")); err == nil {
+		addr1, err := mail.ParseAddress(addr.String())
+		if false {
+			// https://github.com/golang/go/issues/11292
+			// https://github.com/golang/go/issues/11293
+			// https://github.com/golang/go/issues/11294
+			if err != nil {
+				panic(err)
+			}
+			if !reflect.DeepEqual(addr, addr1) {
+				panic("addr changed")
+			}
+		}
+	}
+	io.Copy(ioutil.Discard, msg.Body)
+	return 1
+}
