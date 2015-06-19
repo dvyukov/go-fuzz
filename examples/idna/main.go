0e8e5d5db7923338638cdd89820f4794b145ef66
Dmitry Vyukov <dvyukov@google.com>
2015-06-19 17:39:58 +0200

    add idna test

---
+++ examples/idna/main.go
@@ -0,0 +1,40 @@
+package idna
+
+import (
+	"fmt"
+
+	"golang.org/x/net/idna"
+)
+
+func Fuzz(data []byte) int {
+	for _, v := range data {
+		if v <= 0x20 || v >= 0x80 {
+			return 0
+		}
+	}
+	dec, err := idna.ToUnicode(string(data))
+	if err != nil {
+		return 0
+	}
+	enc, err := idna.ToASCII(dec)
+	if err != nil {
+		fmt.Printf("data: %q\n", data)
+		fmt.Printf("dec : %q\n", dec)
+		panic(err)
+	}
+	dec1, err := idna.ToUnicode(enc)
+	if err != nil {
+		fmt.Printf("data: %q\n", data)
+		fmt.Printf("dec : %q\n", dec)
+		fmt.Printf("enc : %q\n", enc)
+		panic(err)
+	}
+	if dec != dec1 {
+		fmt.Printf("data: %q\n", data)
+		fmt.Printf("dec : %q\n", dec)
+		fmt.Printf("enc : %q\n", enc)
+		fmt.Printf("dec1: %q\n", dec1)
+		panic("not equal")
+	}
+	return 1
+}
