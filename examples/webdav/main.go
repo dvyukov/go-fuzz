7287c86b98a171f4c3a74b6108a7cf93ca18a169
Dmitry Vyukov <dvyukov@google.com>
2015-06-20 11:23:19 +0200

    add webdav test

---
+++ examples/webdav/main.go
@@ -0,0 +1,45 @@
+package webdav
+
+import (
+	"bufio"
+	"bytes"
+	"net/http"
+
+	"golang.org/x/net/webdav"
+)
+
+func Fuzz(data []byte) int {
+	score := 0
+	buf := bufio.NewReader(bytes.NewReader(data))
+	for {
+		r, err := http.ReadRequest(buf)
+		if err != nil {
+			break
+		}
+		h := &webdav.Handler{
+			FileSystem: webdav.NewMemFS(),
+			LockSystem: webdav.NewMemLS(),
+		}
+		w := &NilWriter{hdr: make(http.Header)}
+		h.ServeHTTP(w, r)
+		score = 1
+	}
+	return score
+}
+
+type NilWriter struct {
+	hdr  http.Header
+	code int
+}
+
+func (w *NilWriter) Header() http.Header {
+	return w.hdr
+}
+
+func (w *NilWriter) Write(data []byte) (int, error) {
+	return len(data), nil
+}
+
+func (w *NilWriter) WriteHeader(code int) {
+	w.code = code
+}
