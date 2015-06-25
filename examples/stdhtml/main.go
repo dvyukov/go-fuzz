f713f92aa44ebb0b978492dc0bfb95fbdedf11de
Dmitry Vyukov <dvyukov@google.com>
2015-06-26 00:06:55 +0200

    add html test

---
+++ examples/stdhtml/main.go
@@ -0,0 +1,21 @@
+package stdhtml
+
+import (
+	"fmt"
+	"html"
+)
+
+func Fuzz(data []byte) int {
+	s0 := string(data)
+	s1 := html.EscapeString(s0)
+	s2 := html.UnescapeString(s1)
+	if s0 != s2 {
+		fmt.Printf("s0: %q\n", s0)
+		fmt.Printf("s1: %q\n", s1)
+		fmt.Printf("s2: %q\n", s2)
+		panic("string changed")
+	}
+	s3 := html.UnescapeString(s0)	
+	html.EscapeString(s3)
+	return 0
+}
