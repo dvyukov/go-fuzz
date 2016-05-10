9fd2e91021696c41b9b616c096169469acdc3417
Dmitry Vyukov <dvyukov@google.com>
2016-05-10 10:09:33 +0200

    fix and extend snappy test

--- examples/snappy/main.go
+++ examples/snappy/main.go
@@ -4,7 +4,12 @@
 package snappy
 
 import (
-	"github.com/golang/snappy/snappy"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/golang/snappy"
 )
 
 func Fuzz(data []byte) int {
@@ -27,12 +32,112 @@ func Fuzz(data []byte) int {
 		panic("bad decoded len")
 	}
 	n = snappy.MaxEncodedLen(len(dec))
-	enc, err := snappy.Encode(nil, dec)
-	if err != nil {
-		panic(err)
-	}
+	enc := snappy.Encode(nil, dec)
 	if len(enc) > n {
 		panic("bad encoded len")
 	}
+
+	dec1, err := snappy.Decode(nil, enc)
+	if err != nil {
+		panic(err)
+	}
+	if bytes.Compare(dec, dec1) != 0 {
+		panic("not equal")
+	}
+	return 1
+}
+
+func FuzzFraming(data []byte) int {
+	r := snappy.NewReader(bytes.NewReader(data))
+	buf := make([]byte, 0, 1023)
+	dec := make([]byte, 0, 1024)
+	for i := 0; ; i++ {
+		x := i
+		if x > cap(buf) {
+			x = cap(buf)
+		}
+		n, err := r.Read(buf[:x])
+		if n != 0 {
+			dec = append(dec, buf[:n]...)
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0
+		}
+	}
+	r.Reset(bytes.NewReader(data))
+	dec1, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	if bytes.Compare(dec, dec1) != 0 {
+		fmt.Printf("dec0: %q\n", dec)
+		fmt.Printf("dec1: %q\n", dec1)
+		panic("not equal")
+	}
+
+	bufw := new(bytes.Buffer)
+	w := snappy.NewBufferedWriter(bufw)
+	for i := 0; len(dec1) > 0; i++ {
+		x := i
+		if x > len(dec1) {
+			x = len(dec1)
+		}
+		n, err := w.Write(dec1[:x])
+		if n != x {
+			panic("short write")
+		}
+		if err != nil {
+			panic(err)
+		}
+		dec1 = dec1[x:]
+		if (i % 2) != 0 {
+			w.Flush()
+		}
+	}
+	w.Close()
+
+	dec1 = append([]byte{}, dec...)
+	bufw2 := new(bytes.Buffer)
+	w2 := snappy.NewWriter(bufw2)
+	for i := 2; len(dec1) > 0; i++ {
+		x := i
+		if x > len(dec1) {
+			x = len(dec1)
+		}
+		n, err := w2.Write(dec1[:x])
+		if n != x {
+			panic("short write")
+		}
+		if err != nil {
+			panic(err)
+		}
+		dec1 = dec1[x:]
+		if (i % 2) != 0 {
+			w2.Flush()
+		}
+	}
+	w2.Close()
+
+	r2 := snappy.NewReader(bufw)
+	dec2, err := ioutil.ReadAll(r2)
+	if err != nil {
+		panic(err)
+	}
+	if bytes.Compare(dec, dec2) != 0 {
+		panic("not equal")
+	}
+
+	r3 := snappy.NewReader(bufw2)
+	dec3, err := ioutil.ReadAll(r3)
+	if err != nil {
+		panic(err)
+	}
+	if bytes.Compare(dec, dec3) != 0 {
+		panic("not equal")
+	}
+
 	return 1
 }
