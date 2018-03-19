a865ffbc66599aab8c5a4f220a548df8a38e8037
Dmitry Vyukov <dvyukov@google.com>
2018-03-19 10:42:49 +0100

    slides: add Fuzzing: the new unit testing slides
    From GopherCon Russia 2018.

--- slides/README.md
+++ slides/README.md
@@ -1,1 +1,4 @@
-### [View the slides](http://go-talks.appspot.com/github.com/dvyukov/go-fuzz/slides/go-fuzz.slide)
+# go-fuzz presentations
+
+- [go-fuzz: randomized testing system for Go](http://go-talks.appspot.com/github.com/dvyukov/go-fuzz/slides/go-fuzz.slide) (extended version of GopherCon 2015 talk)
+- [Fuzzing: the new unit testing](http://go-talks.appspot.com/github.com/dvyukov/go-fuzz/slides/fuzzing.slide) (GopherCon Russia 2018)
---
+++ slides/regexp.go
@@ -0,0 +1,27 @@
+package regexp
+
+import (
+	"regexp"
+)
+
+// START OMIT
+func FuzzRegexp(data []byte) int {
+	if len(data) < 3 {
+		return 0
+	}
+	longestMode := data[0]%2 != 0  // first byte as "longest" flag
+	reStr := data[1 : len(data)/2] // half as regular expression
+	matchStr := data[len(data)/2:] // the rest is string to match
+
+	re, err := regexp.Compile(string(reStr))
+	if err != nil {
+		return 0
+	}
+	if longestMode {
+		re.Longest()
+	}
+	re.FindAll(matchStr, -1)
+	return 0
+}
+
+// END OMIT
