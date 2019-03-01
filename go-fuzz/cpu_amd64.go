1baf3aecfdd3e83c57229baa4039ee529f7ffe53
Josh Bleecher Snyder <josharian@gmail.com>
2019-03-01 14:29:55 -0800

    go-fuzz: add SSE2 and AVX2 implementations of compareCoverBody
    The SSE2 implementation uses only instructions that are assumed
    available by the Go runtime. It can thus be used anywhere.
    
    AVX2 support is checked for at runtime.
    
    Benchmark Go implementation vs previous asm
    
    name                     old time/op  new time/op  delta
    CompareCoverBody/easy-8  6.79ns ± 9%  3.35ns ± 1%  -50.73%  (p=0.000 n=10+10)
    CompareCoverBody/hard-8  18.6µs ± 5%   9.1µs ± 3%  -51.00%  (p=0.000 n=10+10)
    
    Benchmark previous asm vs SSE2 asm
    
    name                     old time/op  new time/op  delta
    CompareCoverBody/easy-8  3.35ns ± 1%  5.56ns ± 5%  +66.01%  (p=0.000 n=10+10)
    CompareCoverBody/hard-8  9.12µs ± 3%  1.79µs ± 2%  -80.38%  (p=0.000 n=10+9)
    
    Benchmark SSE2 asm vs AVX2 asm
    
    name                     old time/op  new time/op  delta
    CompareCoverBody/easy-8  5.56ns ± 5%  5.35ns ± 2%   -3.82%  (p=0.000 n=10+9)
    CompareCoverBody/hard-8  1.79µs ± 2%  1.05µs ± 2%  -41.36%  (p=0.000 n=9+10)
    
    The regression in the easy benchmarks are almost entirely due
    to the runtime check for AVX2 support.
    It is also not particularly relevant to go-fuzz performance;
    the hard case is both more typical and more important.

---
+++ go-fuzz/cpu_amd64.go
@@ -0,0 +1,39 @@
+// Copyright 2015 Dmitry Vyukov. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package main
+
+// Adapted from GOROOT/src/internal/cpu/cpu_x86.go.
+
+var hasAVX2 bool
+
+func cpuid(eaxArg, ecxArg uint32) (eax, ebx, ecx, edx uint32)
+func xgetbv() (eax, edx uint32)
+
+const (
+	// ecx bits
+	cpuid_OSXSAVE = 1 << 27
+
+	// ebx bits
+	cpuid_AVX2 = 1 << 5
+)
+
+func init() {
+	_, _, ecx1, _ := cpuid(1, 0)
+	hasOSXSAVE := cpuBitIsSet(ecx1, cpuid_OSXSAVE)
+
+	osSupportsAVX := false
+	// For XGETBV, OSXSAVE bit is required and sufficient.
+	if hasOSXSAVE {
+		eax, _ := xgetbv()
+		// Check if XMM and YMM registers have OS support.
+		osSupportsAVX = cpuBitIsSet(eax, 1<<1) && cpuBitIsSet(eax, 1<<2)
+	}
+
+	_, ebx7, _, _ := cpuid(7, 0)
+	hasAVX2 = cpuBitIsSet(ebx7, cpuid_AVX2) && osSupportsAVX
+}
+
+func cpuBitIsSet(hwc uint32, value uint32) bool {
+	return hwc&value != 0
+}
