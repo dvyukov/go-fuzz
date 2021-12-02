829e0cb31732d5853eb1b5da73cf964d42aed1e1
thepudds <thepudds@example.com>
2021-12-02 10:47:22 -0500

    go-fuzz: set fd inheritance properly for Go 1.17+ on Windows
    During Go 1.17 development, fd inheritance on Windows was changed in:
        CL 288297 - "syscall: restrict inherited handles on Windows"
        https://golang.org/cl/288297
    
    Running go-fuzz with Go 1.17 on at least some Windows versions caused errors like:
        "write to testee failed: write |1: The pipe is being closed"
    
    The fix is to properly set SysProcAttr.AdditionalInheritedHandles, which is modeled
    after the suggestion from Jason Donenfeld in CL 320050:
        https://go-review.googlesource.com/c/go/+/320050/-1..3#message-ed1be75fda3d32c5ff2bd037b951a875cb07c3db
    
    Fixes #328

---
+++ go-fuzz/sys_go117_windows.go
@@ -0,0 +1,31 @@
+// Copyright 2015 go-fuzz project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+//go:build go1.17
+// +build go1.17
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func setupCommMapping(cmd *exec.Cmd, comm *Mapping, rOut, wIn *os.File) {
+	syscall.SetHandleInformation(syscall.Handle(comm.mapping), syscall.HANDLE_FLAG_INHERIT, 1)
+	syscall.SetHandleInformation(syscall.Handle(rOut.Fd()), syscall.HANDLE_FLAG_INHERIT, 1)
+	syscall.SetHandleInformation(syscall.Handle(wIn.Fd()), syscall.HANDLE_FLAG_INHERIT, 1)
+	// Setting AdditionalInheritedHandles is required in Go 1.17+.
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		AdditionalInheritedHandles: []syscall.Handle{
+			syscall.Handle(wIn.Fd()),
+			syscall.Handle(rOut.Fd()),
+			syscall.Handle(comm.mapping),
+		},
+	}
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_COMM_FD=%v", comm.mapping))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_IN_FD=%v", rOut.Fd()))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_OUT_FD=%v", wIn.Fd()))
+}
--- go-fuzz/sys_windows.go
+++ go-fuzz/sys_windows.go
@@ -4,10 +4,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"reflect"
 	"syscall"
 	"unsafe"
@@ -46,13 +44,4 @@ func createMapping(name string, size int) (*Mapping, []byte) {
 func (m *Mapping) destroy() {
 	syscall.UnmapViewOfFile(m.addr)
 	syscall.CloseHandle(m.mapping)
-}
-
-func setupCommMapping(cmd *exec.Cmd, comm *Mapping, rOut, wIn *os.File) {
-	syscall.SetHandleInformation(syscall.Handle(comm.mapping), syscall.HANDLE_FLAG_INHERIT, 1)
-	syscall.SetHandleInformation(syscall.Handle(rOut.Fd()), syscall.HANDLE_FLAG_INHERIT, 1)
-	syscall.SetHandleInformation(syscall.Handle(wIn.Fd()), syscall.HANDLE_FLAG_INHERIT, 1)
-	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_COMM_FD=%v", comm.mapping))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_IN_FD=%v", rOut.Fd()))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("GO_FUZZ_OUT_FD=%v", wIn.Fd()))
 }
